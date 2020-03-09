4b9bbdedf4bf2397b083296fb24e8b52c168f70c
Adan Ruiz <ruizoswin292@gmail.com>
2020-03-09 19:45:42 +0000

    Primer commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,16 @@
 #
-# Cookbook:: webserver
+#
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+package 'apache2' do
+  action :install
+end
+file '/var/www/html/index.html' do
+  content 'My first cookbook'
+  action :create
+end
+
+service 'apache2' do
+  action [ :start, :enable ]
+end
